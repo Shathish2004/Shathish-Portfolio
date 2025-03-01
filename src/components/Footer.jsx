34bb9a752e5e4ac54ea6af7d9171ce5edb639a28
Shathish2004 <shathishkumaran07@gmail.com>
2025-03-01 18:53:29 +0530

    modified the footer font weight

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
   ];
 
   return (
-    <footer className="max-w-6xl mx-auto border-t-1 border-gray-900 dark:border-white py-6 px-4 mt-10">
+    <footer className="max-w-6xl mx-auto border-t-1 font-medium border-gray-900 dark:border-white py-6 px-4 mt-10">
       <div className="container mx-auto flex flex-col gap-y-5 justify-between items-center">
         {/* Center Section - Navigation */}
         <ul className="grid grid-cols-2 text-center space-y-3 sm:flex space-x-4 mt-2 text-[15px] sm:text-[20px] cursor-pointer sm:mt-0">
