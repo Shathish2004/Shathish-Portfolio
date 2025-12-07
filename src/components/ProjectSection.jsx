30f0383d426f78db057ee166fd231cb0877d5678
Shathish2004 <shathishkumaran07@gmail.com>
2025-12-07 08:58:35 +0530

    added the project video url in the project Section

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -14,7 +14,7 @@ const projectsData = [
         category: "Full Stack",
         description: "A production-ready e-commerce platform featuring OTP-secured authentication, real-time order tracking, and an intelligent return-management system. Includes a full admin dashboard for managing products, users, orders, and analytics.",
         tech: ["React", "Node.js", "MongoDB", "Redux Toolkit", "NodeMailer"],
-        video: "https://res.cloudinary.com/dxdqojwlo/video/upload/v1763959657/EcommerceAPP_lgaau5.mp4",
+        video: "https://res.cloudinary.com/dylmrhy5h/video/upload/v1765077872/EcommerceAPP_vu1v3l.mp4",
         github: "https://github.com/SHATHISH-07/Ecommerce-App",
         live: "https://shathish-07.github.io/ecommerce-frontend/#/",
         color: "#2a0878"
@@ -26,7 +26,7 @@ const projectsData = [
         category: "AI & Real-time",
         description: "An AI-driven voice-interactive learning platform that delivers real-time conversational tutoring using Vapi AI. Supports dynamic lesson generation, secure authentication, and progress tracking for personalized learning.",
         tech: ["Next.js", "TypeScript", "Supabase", "Clerk", "Vapi (Voice AI)"],
-        video: "https://res.cloudinary.com/dxdqojwlo/video/upload/v1763959653/ReactIQ_bqpdgb.mp4",
+        video: "https://res.cloudinary.com/dylmrhy5h/video/upload/v1765077865/ReactIQ_engeyf.mp4",
         github: "https://github.com/SHATHISH-07/ai-saas-app",
         live: "https://ai-saas-app-bice-one.vercel.app/",
         color: "#1a1a1a"
@@ -38,7 +38,7 @@ const projectsData = [
         category: "Data Visualization",
         description: "A responsive weather intelligence app offering real-time forecasts, air quality data, and geo-based insights for global locations. Optimized for performance with advanced API integration and state management.",
         tech: ["React", "TypeScript", "Tailwind", "LocationIQ API", "Redux"],
-        video: "https://res.cloudinary.com/dxdqojwlo/video/upload/v1763959668/WeatherApp_jrtumf.mp4",
+        video: "https://res.cloudinary.com/dylmrhy5h/video/upload/v1765077856/WeatherApp_qg38af.mp4",
         github: "https://github.com/SHATHISH-07/Weather-app",
         live: "https://shathish-07.github.io/weatherapp-frontend/#/",
         color: "#4a1d96"
@@ -50,7 +50,7 @@ const projectsData = [
         category: "Media Platform",
         description: "A movie and series discovery platform powered by the TMDB API, featuring trending titles, cast details, and secure user authentication. Designed with responsive UI and protected routes for a seamless browsing experience.",
         tech: ["MERN Stack", "Tailwind CSS", "JWT Auth", "TMDB API"],
-        video: "https://res.cloudinary.com/dxdqojwlo/video/upload/v1763959673/IMDBApp_tcdcjx.mp4",
+        video: "https://res.cloudinary.com/dylmrhy5h/video/upload/v1765077868/IMDBApp_lcc6ke.mp4",
         github: "https://github.com/SHATHISH-07/Projects/tree/main/FullStack_Project/IMDB-CLONE",
         live: "https://shathish-07.github.io/IMDB-frontend/#/",
         color: "#0f172a"
